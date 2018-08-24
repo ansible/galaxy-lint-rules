f0cb0b5dc0a2bef713b770945791d258100ba379
Andrew Crosby <acrosby@redhat.com>
2018-08-23 21:29:27 -0400

    removed 2nd error msg on sudo, added 2 deprecation rules

---
+++ rules/WithLoopRule.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2018 Ansible, Inc.
+# All Rights Reserved.
+
+from ansiblelint import AnsibleLintRule
+
+
+class WithLoopRule(AnsibleLintRule):
+    id = 'GALAXYTEST106'
+    shortdesc = 'Deprecated with_X for loops'
+    description = """With the release of Ansible 2.5, the recommended way to
+    perform loops is to use the new loop keyword instead of with_X style loops
+    For more details see:
+     https://docs.ansible.com/ansible/2.6/porting_guides/porting_guide_2.5.html
+    """
+    tags = ['deprecated']
+
+    def matchtask(self, file, task):
+        return ' with_' in task
