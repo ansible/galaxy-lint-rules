74160e131e1a8ab7af0fc20c2c031ccf672787e7
Andrew Crosby <acrosby@redhat.com>
2018-10-16 11:37:55 -0400

    Added rule to check for present metadata

---
+++ rules/MetaMainHasInfoRule.py
@@ -0,0 +1,35 @@
+from ansiblelint import AnsibleLintRule
+
+
+class MetaMainHasInfoRule(AnsibleLintRule):
+    id = '701GAL'
+    shortdesc = 'meta/main.yml should contain relevant info'
+    info = ['author', 'description', 'company',
+            'license', 'min_ansible_version',
+            'platforms', 'galaxy_tags']
+    description = 'meta/main.yml should contain: ' + ', '.join(info)
+    tags = ['metadata']
+
+    def matchplay(self, file, data):
+        if file['type'] != 'meta':
+            return False
+
+        galaxy_info = data.get('galaxy_info', None)
+        if not galaxy_info:
+            return [({'meta/main.yml': data},
+                    "No 'galaxy_info' found")]
+
+        results = []
+        for info in self.info:
+            if not galaxy_info.get(info, None):
+                results.append(({'meta/main.yml': data},
+                                'Role info should contain %s' % info))
+
+        platforms = galaxy_info.get('platforms', None)
+        if platforms:
+            for platform in platforms:
+                if not platform.get('name', None):
+                    results.append(({'meta/main.yml': data},
+                                    'Platform should contain name'))
+
+        return results
